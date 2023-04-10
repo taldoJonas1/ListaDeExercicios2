3973e24d95d9e36d171fe03da2d10462ad469f1d
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-04-10 18:12:06 -0300

    Update lista2.js

--- lista2.js
+++ lista2.js
@@ -1,11 +1,35 @@
+function ex1(){
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    let n3 = Number(document.getElementById("n3").value)
+    let n4 = Number(document.getElementById("n4").value)
+    let media
+    let resultado
+
+    media = (n1 + n2 + n3 + n4) / 4
+    if (media >= 7){
+        resultado = "Aprovado"
+    }
+    else {
+        resultado = "Reprovado"
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
+
+function ex2(){
+    
+}
+
 function ex5(){
-    let n1 = Number (document.getElementById("n1").value)
-    let n2 = Number (document.getElementById("n2").value)
-    let escolha = Number (document.getElementById("escolha").value)
+    // recupera nro1, nro2, escolha 
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)
+    let escolha = Number(document.getElementById("escolha").value)
+    // vamos verificar o que o usuário escolheu
     let resultado
     switch(escolha){
         case 1: resultado = (n1 + n2) / 2
-                break //nao entra nos cases de baixo
+                break // não entra nos cases de baixo
         case 2: if (n1 >= n2){
                     resultado = n1 - n2
                 }
@@ -13,16 +37,17 @@ function ex5(){
                     resultado = n2 - n1
                 }
                 break
-        case 3: (resultado = n1 * n2)
+        case 3: resultado = n1 * n2
                 break
-        case 4: if (resultado != 0){
-                (resultado = n1 / n2)
+        case 4: if (nro2 != 0) {
+                    resultado = n1 / n2
                 }
                 else {
-                    resultado = "Não pode dividir por 0"
+                    resultado = "Não pode dividir por zero"
                 }
                 break
         default: resultado = "Opção inválida"
     }
+    // saída do dados
     document.getElementById("resultado").innerHTML = resultado
 }
